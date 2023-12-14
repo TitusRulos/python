16c03f50d50f602f65629e82ceb9fca1d8eaf124
TitusRulos <jujotiga@gmail.com>
2023-12-14 16:52:16 -0500

    nuevos archivos: ejercicios.py

---
+++ ejercicios.py
@@ -0,0 +1,55 @@
+#Ejercicio 1.
+    #n=str(input("ingresa tu nombre: "))
+    #print("Hola", n)
+#Ejercicio 2.
+    #import math as mt
+    #r=float(input("ingrese el radio: "))
+    #p=(2*(mt.pi))*r
+    #a=(mt.pi)*((r)**2)
+    #print("El perímetro es ", p)
+    #print("El área es ", a)
+#Ejercicio 3.
+    #a=float(input("Primer nota: "))
+    #b=float(input("Segunda nota: "))
+    #c=float(input("Tercera nota: "))
+    #d=float(input("Cuarta nota: "))
+    #prom=((a+b+c+d)/(4))
+    #print(prom)
+#Ejercicio 4.
+    #cm=float(input("Ingrese longitud: "))
+    #IN=cm/2.54
+    #print(int(cm), "cm =45", IN,"in"  )
+#Ejercicio 5.
+    #c1=float(input("Ingrese cateto a: "))
+    #c2=float(input("Ingrese cateto b: "))
+    #h=(((c1)**2)+((c2)**2))**(1/2)
+    #print("La hipotenusa es ",h)
+#Ejercicio 6.
+    #t=int(input("Hora actual: "))
+    #h=int(input("Cantidad de horas: "))
+    #tFutura=((t+h)%24)
+    #print("En {} horas, el reloj marcara las {}".format(t, tFutura))
+#Ejercicio 7.
+ #import math as mt
+ #num=float(input("Ingrese un número: "))
+ #Entera=mt.trunc(num)
+ #rta=(abs(num)-Entera)
+ #print(round(rta,1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000))
+#Ejercicio 8. 
+ #nota1=int(input("ingrese nota certamen 1: "))
+ #nota2=int(input("ingrese nota certamen 2: "))
+ #lab=int(input("ingrese nota laboratorio: "))
+ #nota3=((((60-(lab*0.3))*30)/7)-(nota1+nota2))
+ #print(int(nota3))
+#Ejercicios 9.
+palabra1=str(input("palabra 1: "))
+palabra2=str(input("palabra 2: "))
+
+
+
+
+
+
+
+
+
