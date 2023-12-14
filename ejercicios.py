edc5e63f82050ca1dda0c494ff4ee6072021ee1f
TitusRulos <jujotiga@gmail.com>
2023-12-14 17:49:04 -0500

    modificados:     ejercicios.py

--- ejercicios.py
+++ ejercicios.py
@@ -1,6 +1,7 @@
 #Ejercicio 1.
     #n=str(input("ingresa tu nombre: "))
     #print("Hola", n)
+
 #Ejercicio 2.
     #import math as mt
     #r=float(input("ingrese el radio: "))
@@ -8,6 +9,7 @@
     #a=(mt.pi)*((r)**2)
     #print("El perímetro es ", p)
     #print("El área es ", a)
+
 #Ejercicio 3.
     #a=float(input("Primer nota: "))
     #b=float(input("Segunda nota: "))
@@ -15,35 +17,74 @@
     #d=float(input("Cuarta nota: "))
     #prom=((a+b+c+d)/(4))
     #print(prom)
+
 #Ejercicio 4.
     #cm=float(input("Ingrese longitud: "))
     #IN=cm/2.54
     #print(int(cm), "cm =45", IN,"in"  )
+
 #Ejercicio 5.
     #c1=float(input("Ingrese cateto a: "))
     #c2=float(input("Ingrese cateto b: "))
     #h=(((c1)**2)+((c2)**2))**(1/2)
     #print("La hipotenusa es ",h)
+
 #Ejercicio 6.
     #t=int(input("Hora actual: "))
     #h=int(input("Cantidad de horas: "))
     #tFutura=((t+h)%24)
     #print("En {} horas, el reloj marcara las {}".format(t, tFutura))
+
 #Ejercicio 7.
  #import math as mt
  #num=float(input("Ingrese un número: "))
  #Entera=mt.trunc(num)
  #rta=(abs(num)-Entera)
  #print(round(rta,1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000))
+
 #Ejercicio 8. 
  #nota1=int(input("ingrese nota certamen 1: "))
  #nota2=int(input("ingrese nota certamen 2: "))
  #lab=int(input("ingrese nota laboratorio: "))
  #nota3=((((60-(lab*0.3))*30)/7)-(nota1+nota2))
- #print(int(nota3))
+ #print("Necesita nota", int(nota3), "en el certamen 3")
+
 #Ejercicios 9.
-palabra1=str(input("palabra 1: "))
-palabra2=str(input("palabra 2: "))
+ #palabra1=str(input("palabra 1: "))
+ #palabra2=str(input("palabra 2: "))
+ #if len(palabra1)>len(palabra2):
+ #    dif=(len(palabra1)-(len(palabra2)))
+ #    print("la palabra {} tiene {} letras de mas que {}.".format(palabra1, dif, palabra2))
+ #elif len(palabra2)>len(palabra1):
+ #    dif=(len(palabra2)-(len(palabra1)))
+ #    print("la palabra {} tiene {} letras de mas que {}.".format(palabra2, dif, palabra1))
+ #else:
+ #    print("Las dos palabras tienen el mismo largo")
+
+# Ejercicio 10.
+    # import datetime as dt
+    # print("Ingrese su nacimiento")
+    # dia=int(input("Día: "))
+    # mes=int(input("Mes: "))
+    # año=int(input("Año: "))
+    # diaActual=dt.day
+    # print(diaActual)
+    # mesActual=int(dt.month  )
+    # añoActual=int(dt.year)
+    # edad=añoActual-año
+    # if mesActual>mes:
+    #     edad=edad-1
+    # elif mesActual==mes and diaActual>dia:
+    #     edad=edad-1
+    # else:
+    #     print(edad)
+    # print(edad)
+
+
+    
+
+
+
 
 
 
